ea1b7512c545d732ab56d77e7863cdd51b228422
Lindsay Ciastko <LindsayCiastko@gmail.com>
2018-07-11 16:08:38 -0700

    Feat: Added initial movements for the bugs
    Added movements to the enemy objects.

--- js/app.js
+++ js/app.js
@@ -23,26 +23,30 @@ We want range of Y values to be between this.dy - 20 and this.dy * 3 - 20;
 */
 
 class Enemy extends Entity {
-  constructor() {
+  constructor(x, y, speed) {
     super();
     this.sprite = "images/enemy-bug.png";
-    this.x = 0;
-    this.y = this.randomizeStartingLocation();
-    this.speed = 5;
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
   }
   // Update the enemy's position, required method for game
   // Parameter: dt, a time delta between ticks
   update(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    this.x += dt * this.speed * 101;
   }
-  randomizeStartingLocation() {
+}
+
+let allEnemies = [];
+generateEnemy();
+
+function generateEnemy() {
+  for (var i = 0; i < 5; i++) {
+    var speed = Math.floor(Math.random() * 6 + 1);
     let yRandom = Math.floor(Math.random() * 3) + 1;
-    console.log(yRandom);
-    console.log(this.dy);
-    this.y = this.dy * yRandom - 20;
-    return this.y;
+    let y = 83 * yRandom - 20;
+    var x = 0;
+    allEnemies.push(new Enemy(x, y, speed));
   }
 }
 
